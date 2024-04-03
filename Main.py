162faa43f40c3e31c9d7e2188a6b4d88700ed680
DerDev <72467981+SolelyMoose@users.noreply.github.com>
2024-04-03 14:48:36 -0500

    Update 1.0.3 Main.py
    Adds separate file for handling the GUI.

--- Main.py
+++ Main.py
@@ -1,9 +1,20 @@
+import os
+import sys
 import psutil
 import time
 import win32gui
 import win32process
 import tkinter as tk
 from tkinter import ttk
+
+# Determine the directory containing main.py
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Add the directory containing main.py to the sys.path list
+sys.path.append(script_dir)
+
+# Import the gui module from the directory containing main.py
+import gui
 
 # Dictionary to store process IDs and their respective start times
 process_start_times = {}
@@ -45,7 +56,7 @@ def list_visible_processes():
     win32gui.EnumWindows(callback, None)
     return processes
 
-def update_gui():
+def update_gui(opened_processes_list, closed_processes_list):
     print("Updating GUI...")
     opened_processes_list.delete(0, tk.END)
     closed_processes_list.delete(0, tk.END)
@@ -72,43 +83,16 @@ def update_interval_changed():
 
 def main():
     print("Starting main function...")
-    global opened_processes_list, closed_processes_list, update_interval_entry  # Declare variables as global
 
     root = tk.Tk()
     root.title("Process Timer")
 
-    notebook = ttk.Notebook(root)
-    notebook.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
-
-    opened_processes_frame = ttk.Frame(notebook)
-    closed_processes_frame = ttk.Frame(notebook)
-
-    notebook.add(opened_processes_frame, text="Opened Processes")
-    notebook.add(closed_processes_frame, text="Closed Processes")
-
-    opened_processes_list = tk.Listbox(opened_processes_frame, width=50, height=20)
-    opened_processes_list.pack(padx=10, pady=10)
-
-    closed_processes_list = tk.Listbox(closed_processes_frame, width=50, height=20)
-    closed_processes_list.pack(padx=10, pady=10)
-
-    # Update interval input field
-    update_interval_frame = tk.Frame(root)
-    update_interval_frame.pack(pady=(10, 0))
-
-    update_interval_label = tk.Label(update_interval_frame, text="Update Interval (seconds):")
-    update_interval_label.pack(side=tk.LEFT, padx=(5, 0), pady=5)
-
-    update_interval_entry = tk.Entry(update_interval_frame, width=10)
-    update_interval_entry.pack(side=tk.LEFT, padx=(0, 5), pady=5)
-
-    update_interval_button = tk.Button(update_interval_frame, text="Set Interval", command=update_interval_changed, width=10)
-    update_interval_button.pack(side=tk.LEFT, pady=5)
+    opened_processes_list, closed_processes_list, update_interval_entry = gui.setup_gui(root, update_interval_changed, update_gui)
 
     # Update the GUI at regular intervals
     def update():
         print("Running update loop...")
-        update_gui()
+        update_gui(opened_processes_list, closed_processes_list)
         root.after(UPDATE_INTERVAL_MS, update)
 
     root.after(0, update)
